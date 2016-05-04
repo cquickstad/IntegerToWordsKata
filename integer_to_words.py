96da2b9607b909ce0f0fc8641d8b86e06bac5172
Chad Quickstad <cquickstad@gmail.com>
2016-05-03 20:35:22 -0700

    Checking in completed integer to words kata

---
+++ integer_to_words.py
@@ -0,0 +1,88 @@
+# -*- coding: UTF-8 -*-
+
+# Integer To Words
+#
+# Convert integer value to word equivalent similar to how you would write out the number on a check.
+#
+# FYI:
+#
+#       Review english grammar:
+#           http://www.grammarbook.com/numbers/numbers.asp
+#
+#       Review large number names:
+#           http://bmanolov.free.fr/numbers_names.php
+
+
+class IntegerToWordsConverter:
+    def __init__(self):
+        # translations is a dictionary that handles basic cases (i.e. 2=>'two')
+        # and cases which must map because they are special (i.e. 13=>'thirteen' not 'threeteen').
+        # This dictionary is the base of higher recursive calls that handle the billions, thousands, etc.
+        # (i.e. the 'three' in 'three billion').
+        self.translations = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight",
+                             9: "nine", 10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 15: "fifteen",
+                             18: "eighteen", 20: "twenty", 30: "thirty", 40: "forty", 50: "fifty", 60: "sixty",
+                             70: "seventy", 80: "eighty", 90: "ninety"}
+
+        # Assumption: place names in this list are separated by a factor of 1000 in increasing order
+        self.place_names = ["thousand", "million", "billion", "trillion", "quadrillion",
+                            "quintillion", "sextillion", "septillion", "octillion",
+                            "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion",
+                            "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion",
+                            "octodecillion", "novemdecillion", "vigintillion", "unvigintillion",
+                            "duovigintillion", "trevigintillion", "quattuorvigintillion",
+                            "quinvigintillion", "sexvigintillion", "septenvigintillion",
+                            "octovigintillion", "novemvigintillion", "trigintillion",
+                            "untrigintillion", "duotrigintillion"]
+
+        self.max_limit = self.get_place_value(len(self.place_names))
+
+    @staticmethod
+    def div_round_down(n, denominator):
+        return int(n / denominator)
+
+    @staticmethod
+    def get_place_value(place_names_index):
+        return 10 ** ((place_names_index + 1) * 3)
+
+    def compound_teen_to_words(self, n):
+        ones_digit = n - 10
+        return self.translations[ones_digit] + "teen"
+
+    def two_digit_hyphen_to_words(self, n):
+        tens_value = IntegerToWordsConverter.div_round_down(n, 10) * 10
+        ones_value = n % 10
+        return self.translations[tens_value] + "-" + self.translations[ones_value]
+
+    def recursive_conversion_starting_at_place(self, n, place_name, place_value):
+        value_in_place = IntegerToWordsConverter.div_round_down(n, place_value)
+        integer_remainder = n - (value_in_place * place_value)
+        remainder_in_words = (" " + self.to_words(integer_remainder)) if integer_remainder > 0 else ""
+        return self.to_words(value_in_place) + " " + place_name + remainder_in_words
+
+    def hundreds_to_words(self, n):
+        return self.recursive_conversion_starting_at_place(n, "hundred", 100)
+
+    def get_largest_place_name_and_value(self, n):
+        for i, place_name in enumerate(self.place_names):
+            place_limit = self.get_place_value(i + 1)
+            if n < place_limit:
+                return place_name, self.get_place_value(i)
+
+    def large_number_to_words(self, n):
+        place_name, place_value = self.get_largest_place_name_and_value(n)
+        return self.recursive_conversion_starting_at_place(n, place_name, place_value)
+
+    def to_words(self, n):
+        if n in self.translations:
+            return self.translations[n]
+        elif n < 20:
+            return self.compound_teen_to_words(n)
+        elif n < 100:
+            return self.two_digit_hyphen_to_words(n)
+        elif n < 1000:
+            return self.hundreds_to_words(n)
+        elif n < self.max_limit:
+            return self.large_number_to_words(n)
+        else:
+            return "unknown"
---
+++ integer_to_words_test.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import unittest
+import integer_to_words
+
+
+class ToWordsTests(unittest.TestCase):
+    def setUp(self):
+        self.cut = integer_to_words.IntegerToWordsConverter()  # (C)lass (U)nder (T)est
+
+    def test_that_single_digits_return_words(self):
+        test_cases = [(1, "one"), (2, "two"), (3, "three"), (4, "four"), (5, "five"), (6, "six"), (7, "seven"),
+                      (8, "eight"), (9, "nine")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_that_non_teen_two_digits_returns_words(self):
+        test_cases = [(10, "ten"), (11, "eleven"), (12, "twelve")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_that_unique_teens_return_words(self):
+        test_cases = [(13, "thirteen"), (15, "fifteen"), (18, "eighteen")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_that_simple_teens_return_words(self):
+        test_cases = [(14, "fourteen"), (16, "sixteen"), (17, "seventeen"), (19, "nineteen")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_even_tens(self):
+        test_cases = [(10, "ten"), (20, "twenty"), (30, "thirty"), (40, "forty"), (50, "fifty"), (60, "sixty"),
+                      (70, "seventy"), (80, "eighty"), (90, "ninety")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_two_digit_hyphens(self):
+        test_cases = [(21, "twenty-one"), (22, "twenty-two"), (39, "thirty-nine"), (45, "forty-five"),
+                      (53, "fifty-three"), (66, "sixty-six"), (78, "seventy-eight"), (87, "eighty-seven"),
+                      (98, "ninety-eight"), (99, "ninety-nine")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_hundreds(self):
+        test_cases = [(100, "one hundred"), (101, "one hundred one"), (110, "one hundred ten"),
+                      (253, "two hundred fifty-three"), (999, "nine hundred ninety-nine")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_thousands(self):
+        test_cases = [(1000, "one thousand"), (1001, "one thousand one"),
+                      (1999, "one thousand nine hundred ninety-nine"),
+                      (10000, "ten thousand"), (10001, "ten thousand one"),
+                      (23142, "twenty-three thousand one hundred forty-two"),
+                      (19999, "nineteen thousand nine hundred ninety-nine"),
+                      (99999, "ninety-nine thousand nine hundred ninety-nine"),
+                      (745111, "seven hundred forty-five thousand one hundred eleven")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_millions(self):
+        test_cases = [(1000000, "one million"),
+                      (987654321, "nine hundred eighty-seven million "
+                                  "six hundred fifty-four thousand "
+                                  "three hundred twenty-one")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_billions(self):
+        test_cases = [(1000000000, "one billion"),
+                      (789123234001, "seven hundred eighty-nine billion "
+                                     "one hundred twenty-three million "
+                                     "two hundred thirty-four thousand "
+                                     "one")]
+        for n, expected in test_cases:
+            self.assertEqual(expected, self.cut.to_words(n))
+
+    def test_all_place_names(self):
+        p = 1
+        n = 0
+        for _ in self.cut.place_names:
+            p *= 1000
+            n += p
+        n += 116
+        self.assertEqual("one duotrigintillion one untrigintillion one trigintillion one novemvigintillion "
+                         "one octovigintillion one septenvigintillion one sexvigintillion one quinvigintillion "
+                         "one quattuorvigintillion one trevigintillion one duovigintillion one unvigintillion "
+                         "one vigintillion one novemdecillion one octodecillion one septendecillion one sexdecillion "
+                         "one quindecillion one quattuordecillion one tredecillion one duodecillion one undecillion "
+                         "one decillion one nonillion one octillion one septillion one sextillion one quintillion "
+                         "one quadrillion one trillion one billion "
+                         "one million one thousand one hundred sixteen", self.cut.to_words(n))
+
+    def test_unknown_value(self):
+        max_limit = 10 ** 102
+        self.assertEqual("unknown", self.cut.to_words(max_limit))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
